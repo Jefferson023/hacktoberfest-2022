b2de82651aa059e9733e66a7ae37b1e66aa5f484
Rodrigo Saads <rodrigosaads@hotmail.com>
2022-10-22 16:14:14 -0300

    Feito atividade 2

--- Rodrigo/questão1.py
+++ Rodrigo/questão1.py
@@ -1,6 +1,5 @@
 nota1 = int(input("Digite a primeira nota "))
 nota2 = int(input("Digite a segunda nota "))
-nota3 = int(input("Digite a segunda nota "))
-print(nota1,nota2,nota3)
+nota3 = int(input("Digite a terceira nota "))
 resultado = (nota1*2 + nota2*3 + nota3*5)/10
 print(resultado)
