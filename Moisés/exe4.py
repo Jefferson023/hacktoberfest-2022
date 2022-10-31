0366a8c1039979c007b440ad4a14a144feee57d7
Moisés Catônio <moises.catonio@gmail.com>
2022-10-31 18:49:59 -0300

    Exercício 4 feito

---
+++ Moisés/exe4.py
@@ -0,0 +1,10 @@
+num1 = float(input("Indique o primeiro número: "))
+print("")
+num2 = float(input("Indique o segundo número: "))
+print("")
+num3 = float(input("Indique o terceiro número: "))
+print("")
+
+lista = [num1, num2, num3]
+lista.sort()
+print(lista)
